edd1c7d71a822a03412ab0a14465334497eedf3d
pincode69 <pin.a.s.o.aso@gmail.com>
2026-03-17 12:32:12 +0200

    change icon and settings for black version

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,19 @@
-import { useFonts } from 'expo-font';
-import { SplashScreen } from 'expo-router';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
-import AppNavigator from './app-navigator';
+import { MASTER_ANDROID } from "@/MASTER_ANDROID";
+import { useFonts } from "expo-font";
+import { SplashScreen } from "expo-router";
+import { useEffect } from "react";
+import "react-native-reanimated";
+// import AppNavigator from './app-navigator';
 
 export const unstable_settings = {
-  anchor: '(tabs)',
+  anchor: "(tabs)",
 };
 
 export default function RootLayout() {
   const [fontsLoaded] = useFonts({
-    PoppinsBold: require('@/assets/fonts/Poppins-Bold.ttf'),
+    PoppinsBold: require("@/assets/fonts/Poppins-Bold.ttf"),
     // PoppinsRegular: require('@/assets/fonts/Poppins-Regular.ttf'),
-    PoppinsMedium: require('@/assets/fonts/Poppins-Medium.ttf'),
+    PoppinsMedium: require("@/assets/fonts/Poppins-Medium.ttf"),
   });
 
   useEffect(() => {
@@ -25,5 +26,6 @@ export default function RootLayout() {
     return null;
   }
 
-  return <AppNavigator />;
+  return <MASTER_ANDROID />;
+  // return <AppNavigator />;
 }
